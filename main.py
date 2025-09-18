5692afd4172c5ce55e79f05bdc5c7bae6fcabe98
Trem Developer <dev@trem.app>
2025-09-18 15:28:52 +0100

    add blog endpoints to FastAPI application

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
- 
+
+@app.get("/blog_list")
+def blog():
+    return {"data": "blog list"}
+
+@app.get("/blog/{id}")
+def fetch_blog(id: int):
+    return {"data": id}
+
 @app.get("/about")
 def about():
-    return {"data": {"This is a simple FastAPI application."}}
+    return {"data": {"This is a simple FastAPI application."}} 
