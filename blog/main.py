aa5538f25ece6234d5700ae3fc88b5605e741f38
Trem Developer <dev@trem.app>
2025-09-23 12:08:04 -0400

    feat: implement database session management and update blog creation endpoint

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from . import schema, models
 from . database import engine
+from sqlalchemy.orm import Session
+from fastapi import Depends
 
 
 app = FastAPI()
@@ -8,6 +10,17 @@ app = FastAPI()
 # Create tables if they don't exist yet using the database engine
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = Session(bind=engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post("/blog")
-def create(request: schema.Blog):
-    return {"data": f"creating blog with title as {request.title} and body as {request.body}"}
+def create(request: schema.Blog, db : Session = Depends(get_db)):
+    db.add(models.Blog(**request.dict()))
+    db.commit()
+    db.refresh()
+    return db
+    
